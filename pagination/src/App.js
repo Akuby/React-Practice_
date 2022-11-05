4b6da4be704f2d22f1c2324517e0c9c738231137
{jhye0702} <zx2663@naver.com>
2022-11-05 17:50:44 +0900

    페이지네이션 구현 2차

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -19,15 +19,31 @@ class App extends Component {
         {id:2, title:'title-2'},
         {id:1, title:'title-1'},
       ],
-      postPerPage:3
+      postPerPage: 3,
+      currentPage: 1
     }
+  }
+
+  setCurrentPage = (page) => { // pagination 컴포넌트한테 얻어온 값으로 state 값을 변경
+    this.setState({
+      currentPage:page
+    })
+  }
+
+  currentPostList= (totalPostList) => { // 현재 페이지에 노출할 포스트 개수를 정해 state 값을 변경
+    const {currentPage, postPerPage} = this.state
+    const startIdx = (currentPage-1) * postPerPage
+    const endIdx = startIdx + postPerPage
+    const slicedList = totalPostList.slice(startIdx,endIdx)
+    return slicedList;
   }
 
   render() {
     return(
       <div id='App'>
-        <PostList postList={this.state.postList}/>
-        <Pagination total={this.state.postList.length} postPerPage={this.state.postPerPage}/>
+        <PostList postList={this.currentPostList(this.state.postList)}/>
+        <Pagination total={this.state.postList.length} postPerPage={this.state.postPerPage}
+        setCurrentPage={this.setCurrentPage}/> {/*Pagination 컴포넌트로 함수 넘기기*/}
       </div>
     )
   }
--- pagination/src/components/Pagination.jsx
+++ pagination/src/components/Pagination.jsx
@@ -9,7 +9,27 @@ class Pagination extends Component {
     }
   }
 
+  pageClick = (data) => { // 클릭한 페이지네이션 값
+    this.props.setCurrentPage(data) // App.js에서 넘어온 현재 페이지 설정 함수에다가 페이지네이션 값을 넣어 돌려준다.
+  }
+
   render() {
+
+    // 10 / 3 => 3.333 - js, python
+    // 10 / 3 => 3 - c, c++, java
+
+    let pageNumbers = [] // state가 아닌 임시로 만든 배열
+    //구조분해할당
+    const {total, postPerPage} = this.props
+    const endpage = Math.ceil((total) / (postPerPage)) // 4
+    for(let i=1; i<=endpage; i++) {
+      pageNumbers.push(i)
+    }
+
+    const result = pageNumbers.map( // 각각의 pageNumber를 태그로 감싸고, 이벤트 핸들러 등을 부여해서 result로 돌려주는 작업
+      (data, idx) => (<span className='page' key={idx} onClick={()=>this.pageClick(data)}>{data}</span>)
+    )
+
     return(
       <div id='pagination'>
         <div>
@@ -18,6 +38,7 @@ class Pagination extends Component {
         <div>
           총 글 개수 : {this.props.total}
         </div>
+        {result}
       </div>
     )
   }
